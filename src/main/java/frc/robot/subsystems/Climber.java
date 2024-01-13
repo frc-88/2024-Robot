ec3e6e0fe948f99898382f6684f137212b784d2f
Michael Walsh <michael.walsh25026@gmail.com>
2024-01-13 10:22:41 -0500

    Added Open Loop control Commands

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,9 +8,15 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
 
-public class Climber extends SubsystemBase { 
+public class Climber extends SubsystemBase {
+  private DoublePreferenceConstant p_armSpeed = new DoublePreferenceConstant("Arm/Speed", -0.5);
+  
   private final TalonFX m_armLeader = new TalonFX(0, "rio");
   private final TalonFX m_armFollower = new TalonFX(1, "rio");
   
@@ -24,6 +30,24 @@ public class Climber extends SubsystemBase {
     m_armLeader.setControl(m_armRequest.withOutput(speed));
     m_armFollower.setControl(m_armFollowerRequest.withOutput(speed));
   }
+
+  public void run(){
+    set(p_armSpeed.getValue());
+  }
+
+  public void stop(){
+    set(0);
+  }
+
+  public Command runFactory(){
+    return new RunCommand(() -> {run();}, this);
+  }
+
+  public Command stopFactory(){
+    return new InstantCommand(() -> {run();}, this);
+  }
+
+
 
   @Override
   public void periodic() {
