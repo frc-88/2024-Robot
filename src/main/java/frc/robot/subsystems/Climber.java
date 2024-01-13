8a5af8479250a14878afb222f40b5ae7826997dc
Nick Avtges <navtges@gmail.com>
2024-01-13 13:32:23 -0500

    begin motion magic work

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,10 +4,16 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -22,9 +28,37 @@ public class Climber extends SubsystemBase {
   
   private final DutyCycleOut m_armRequest =  new DutyCycleOut(0.0);
   private final DutyCycleOut m_armFollowerRequest = new DutyCycleOut(0.0);
+  // create a Motion Magic request, voltage output
+  private final MotionMagicVoltage m_motionMagic = new MotionMagicVoltage(0);
 
   /** Creates a new Climber. */
-  public Climber() {}
+  public Climber() {
+
+
+  }
+
+  private void configureTalons(TalonFX talon) {
+     TalonFXConfiguration cfg = new TalonFXConfiguration();
+
+    /* Configure current limits */
+    MotionMagicConfigs mm = cfg.MotionMagic;
+    mm.MotionMagicCruiseVelocity = 62.5; // 5 rotations per second cruise
+    mm.MotionMagicAcceleration = 250; // Take approximately 0.25 seconds to reach max vel
+    mm.MotionMagicJerk = 500;  // Take approximately 0.5 seconds to reach max accel 
+
+    Slot0Configs slot0 = cfg.Slot0;
+    slot0.kP = 60;
+    slot0.kI = 0;
+    slot0.kD = 0.1;
+    slot0.kV = 0.12;
+    slot0.kS = 0.25; // Approximately 0.25V to get the mechanism moving
+
+    talon.getConfigurator().apply(cfg);
+  }
+
+  public void setPostion(double position){
+    m_armLeader.setControl(m_motionMagic.withPosition(position));
+  }
 
   public void set(double speed){
     m_armLeader.setControl(m_armRequest.withOutput(speed));
@@ -44,13 +78,12 @@ public class Climber extends SubsystemBase {
   }
 
   public Command stopFactory(){
-    return new InstantCommand(() -> {run();}, this);
+    return new InstantCommand(() -> {stop();}, this);
   }
-
-
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Climber:Arm:Postion", m_armLeader.getPosition().getValueAsDouble());
   }
 }
