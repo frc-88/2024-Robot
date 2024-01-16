3231b0e42a5c342512175e18878695cd5f6ede4a
Aymen Abbassi <AymenNA2026@bridge-rayn.org>
2024-01-16 18:52:10 -0500

    changed preference constants

--- src/main/java/frc/robot/subsystems/Pigeon.java
+++ src/main/java/frc/robot/subsystems/Pigeon.java
@@ -6,17 +6,23 @@ import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class Pigeon {
-    private DoublePreferenceConstant p_PigeonYawCalibration = new DoublePreferenceConstant("Pigeon/InitialYaw", 0);
-    private DoublePreferenceConstant p_PigeonRollCalibration = new DoublePreferenceConstant("Pigeon/InitialRoll", 0);
-    private DoublePreferenceConstant p_PigeonPitchCalibration = new DoublePreferenceConstant("Pigeon/InitialPitch", 0);
-    private DoublePreferenceConstant p_GyroXSensitivityScalar = new DoublePreferenceConstant("Pigeon/GyroSensitivityX", 0);
-    private DoublePreferenceConstant p_GyroYSensitivityScalar = new DoublePreferenceConstant("Pigeon/GyroSensitivityY", 0);
-    private DoublePreferenceConstant p_GyroZSensitivityScalar = new DoublePreferenceConstant("Pigeon/GyroSensitivityZ", 0);
+    private DoublePreferenceConstant p_PigeonYawCalibration;
+    private DoublePreferenceConstant p_PigeonRollCalibration; 
+    private DoublePreferenceConstant p_PigeonPitchCalibration;
+    private DoublePreferenceConstant p_GyroXSensitivityScalar;
+    private DoublePreferenceConstant p_GyroYSensitivityScalar;
+    private DoublePreferenceConstant p_GyroZSensitivityScalar;
     private final double GRAVITY = 9.81;
     private Pigeon2 base;
     
     public Pigeon(int deviceID) {
-        this.base = new Pigeon2(deviceID);
+        p_PigeonYawCalibration = new DoublePreferenceConstant("Pigeon" + deviceID + "/InitialYaw", 0);
+        p_PigeonRollCalibration = new DoublePreferenceConstant("Pigeon" + deviceID + "/InitialRoll", 0);
+        p_PigeonPitchCalibration = new DoublePreferenceConstant("Pigeon" + deviceID + "/InitialPitch", 0);
+        p_GyroXSensitivityScalar = new DoublePreferenceConstant("Pigeon" + deviceID + "/GyroXSensitivity", 0);
+        p_GyroYSensitivityScalar = new DoublePreferenceConstant("Pigeon" + deviceID + "/GyroYSensitivity", 0);
+        p_GyroZSensitivityScalar = new DoublePreferenceConstant("Pigeon" + deviceID + "/GyroZSensitivity", 0);
+        base = new Pigeon2(deviceID);
         configurePigeon();
     }
 
@@ -33,47 +39,47 @@ public class Pigeon {
     }
 
     public void calibratePigeon() {
-        this.base.setYaw(0);
+        base.setYaw(0);
     }
     
     public double getAngle() {
-        return this.base.getAngle();
+        return base.getAngle();
     }
     
     public double getYaw() {
-        return this.base.getYaw().getValue();
+        return base.getYaw().getValue();
     }
     
     public double getRoll() {
-        return this.base.getRoll().getValue();
+        return base.getRoll().getValue();
     }
     
     public double getPitch() {
-        return this.base.getPitch().getValue();
+        return base.getPitch().getValue();
     }
     
     public Rotation2d getRotation2d() {
-        return this.base.getRotation2d();
+        return base.getRotation2d();
     }
 
     public double getAccelerationX() {
-        return this.base.getAccelerationX().getValue() * GRAVITY;
+        return base.getAccelerationX().getValue() * GRAVITY;
     }
     
     public double getAccelerationY() {
-        return this.base.getAccelerationY().getValue() * GRAVITY;
+        return base.getAccelerationY().getValue() * GRAVITY;
     }
     
     public double getAccelerationZ() {
-        return this.base.getAccelerationZ().getValue() * GRAVITY;
+        return base.getAccelerationZ().getValue() * GRAVITY;
     }
    
     public double getYawRate() {
-        return this.base.getRate();
+        return base.getRate();
     }
 
     public void reset() {
-        this.base.reset();
+        base.reset();
     }
 
 }
