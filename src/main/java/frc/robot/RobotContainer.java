377fd33c5e2caf04b69c04bc276078c4eb5ab5e7
Aymen Abbassi <AymenNA2026@bridge-rayn.org>
2024-01-20 13:55:54 -0500

    robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,14 +6,25 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.SwerveDrive;
+import frc.robot.util.controllers.FrskyDriverController;
 
 public class RobotContainer {
+
+  private final SwerveDrive m_drive = new SwerveDrive();
+  private final FrskyDriverController m_driverController = new FrskyDriverController(0);
+
+
   public RobotContainer() {
     configureBindings();
+    configureDefaultCommands();
   }
 
   private void configureBindings() {}
 
+  private void configureDefaultCommands() {
+    m_drive.setDefaultCommand(m_drive.fieldOrientedDriveCommandFactory(m_drive, m_driverController));
+  }
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
