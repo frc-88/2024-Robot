2bf533775d519741fc24ae687796a4c2dae5d221
LEELA <Leeladasti@gmail.com>
2024-01-20 13:53:33 -0500

    data logging

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,14 @@
 
 package frc.robot;
 
+
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
   public RobotContainer() {
+    DataLogManager.start();
     configureBindings();
   }
 
