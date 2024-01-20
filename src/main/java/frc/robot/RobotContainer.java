b127bc60fbdfd9ab25c3c7cbe084b3ea5446aa36
LEELA <Leeladasti@gmail.com>
2024-01-20 13:57:47 -0500

    added joystick data

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,12 +6,14 @@ package frc.robot;
 
 
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
   public RobotContainer() {
     DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
     configureBindings();
   }
 
