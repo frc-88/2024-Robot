532410c327ae6f9c3d58d5b531dc551116e0d834
Aymen Abbassi <AymenNA2026@bridge-rayn.org>
2024-01-30 19:09:54 -0500

    moved requests to subsystem

--- src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -5,14 +5,21 @@ import java.util.function.Supplier;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.utility.PhoenixPIDController;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.util.DriveUtils;
 
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements
@@ -20,10 +27,33 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
  * so it can be used in command-based projects easily.
  */
 public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsystem {
+    private double MaxSpeed = 6; // 6 meters per second desired top speed
+    private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
     private static final double kSimLoopPeriod = 0.005; // 5 ms
     private Notifier m_simNotifier = null;
     private double m_lastSimTime;
     private double m_fieldRelativeOffset = 0;
+    private final SlewRateLimiter filterY = new SlewRateLimiter(500);
+    private final SlewRateLimiter filterX = new SlewRateLimiter(500);
+
+    public static final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
+                                                                     // driving in open loop
+
+    private final SwerveRequest.RobotCentric robotCentric = new SwerveRequest.RobotCentric()
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.FieldCentricFacingAngle snapToAngle = new SwerveRequest.FieldCentricFacingAngle()
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+    private final SwerveRequest.PointWheelsAt pointWheelsAt = new SwerveRequest.PointWheelsAt();
+    private final PhoenixPIDController headingController = new PhoenixPIDController(10, 0, 0) {
+        @Override
+        public double calculate(double measurement, double setpoint, double currentTimestamp) {
+            double output = super.calculate(measurement, setpoint, currentTimestamp);
+            output = MathUtil.clamp(output, -MaxAngularRate / 2, MaxAngularRate / 2);
+            return output;
+        }
+    };
 
     public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency,
             SwerveModuleConstants... modules) {
@@ -31,6 +61,8 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
         if (Utils.isSimulation()) {
             startSimThread();
         }
+        headingController.enableContinuousInput(-Math.PI, Math.PI);
+        snapToAngle.HeadingController = headingController;
     }
 
     public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
@@ -38,6 +70,8 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
         if (Utils.isSimulation()) {
             startSimThread();
         }
+        headingController.enableContinuousInput(-Math.PI, Math.PI);
+        snapToAngle.HeadingController = headingController;
     }
 
     public double getRobotOffset() {
@@ -66,6 +100,33 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
             updateSimState(deltaTime, RobotController.getBatteryVoltage());
         });
         m_simNotifier.startPeriodic(kSimLoopPeriod);
+    }
+
+    public Supplier<SwerveRequest> fieldCentricRequest(CommandXboxController controller) {
+        return () -> drive.withVelocityX(filterX.calculate(DriveUtils.signedPow(controller.getLeftY() * MaxSpeed, 2)))
+                .withVelocityY(-filterY.calculate(DriveUtils.signedPow(controller.getLeftX() * MaxSpeed, 2)))
+                .withRotationalRate(DriveUtils.signedPow(-controller.getRightX() * MaxAngularRate, 2));
+    }
+
+    public Supplier<SwerveRequest> SnapToAngleRequest(CommandXboxController controller, double degrees) {
+        return () -> snapToAngle.withVelocityX(filterX.calculate(controller.getLeftY() * MaxSpeed))
+                .withVelocityY(-filterY.calculate(controller.getLeftX() * MaxSpeed))
+                .withTargetDirection(Rotation2d.fromDegrees(degrees));
+    }
+
+    public Supplier<SwerveRequest> robotCentricRequest(CommandXboxController controller) {
+        return () -> robotCentric.withVelocityX(filterX.calculate(controller.getLeftY() * MaxSpeed))
+                .withVelocityY(-filterY.calculate(controller.getLeftX() * MaxSpeed))
+                .withRotationalRate(-controller.getRightX() * MaxAngularRate);
+    }
+
+    public Supplier<SwerveRequest> brakeRequest() {
+        return () -> brake;
+    }
+
+    public Supplier<SwerveRequest> pointWheelsAtRequest() {
+        return () -> pointWheelsAt.withModuleDirection(
+                Rotation2d.fromDegrees(getModule(0).getCANcoder().getAbsolutePosition().getValueAsDouble() * 360));
     }
 
     @Override
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,14 +5,7 @@
 package frc.robot;
 
 import com.ctre.phoenix6.Utils;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.mechanisms.swerve.utility.PhoenixPIDController;
 
-import java.util.function.Supplier;
-
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -21,68 +14,32 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.generated.TunerConstants;
-import frc.robot.util.DriveUtils;
 
 public class RobotContainer {
     private double MaxSpeed = 6; // 6 meters per second desired top speed
     private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
-    private final SlewRateLimiter filterY = new SlewRateLimiter(500);
-    private final SlewRateLimiter filterX = new SlewRateLimiter(500);
-
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
     private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
 
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
-                                                                     // driving in open loop
-
-    private final SwerveRequest.RobotCentric robotCentric = new SwerveRequest.RobotCentric()
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.FieldCentricFacingAngle snapToAngle = new SwerveRequest.FieldCentricFacingAngle()
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
-    private final SwerveRequest.PointWheelsAt pointWheelsAt = new SwerveRequest.PointWheelsAt();
-    private final PhoenixPIDController headingController = new PhoenixPIDController(10, 0, 0) {
-        @Override
-        public double calculate(double measurement, double setpoint, double currentTimestamp) {
-            double output = super.calculate(measurement, setpoint, currentTimestamp);
-            output = MathUtil.clamp(output, -MaxAngularRate / 2, MaxAngularRate / 2);
-            return output;
-        }
-    };
-
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
     private void configureBindings() {
-        drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
-                drivetrain.applyRequest(
-                        () -> drive
-                                .withVelocityX(
-                                        filterX.calculate(DriveUtils.signedPow(joystick.getLeftX() * MaxSpeed, 2)))
-                                .withVelocityY(
-                                        -filterY.calculate(DriveUtils.signedPow(joystick.getLeftY() * MaxSpeed, 2)))
-                                .withRotationalRate(DriveUtils.signedPow(-joystick.getRightX() * MaxAngularRate, 2))));
 
-        headingController.enableContinuousInput(-Math.PI, Math.PI);
-        snapToAngle.HeadingController = headingController;
+        drivetrain.setDefaultCommand(drivetrain.applyRequest(drivetrain.fieldCentricRequest(joystick))); // Drivetrain
+                                                                                                         // will execute
+                                                                                                         // this command
+                                                                                                         // periodically
 
-        joystick.b().whileTrue(drivetrain.applyRequest(getSnapToAngleRequest(270)));
-        joystick.x().whileTrue(drivetrain.applyRequest(getSnapToAngleRequest(90)));
-        joystick.y().whileTrue(drivetrain.applyRequest(getSnapToAngleRequest(0)));
-        joystick.a().whileTrue(drivetrain.applyRequest(getSnapToAngleRequest(180)));
-        isNotMoving().whileTrue(drivetrain
-                .applyRequest(() -> pointWheelsAt.withModuleDirection(Rotation2d.fromDegrees(
-                        (drivetrain.getModule(0).getCANcoder().getAbsolutePosition().getValueAsDouble() * 360)))));
+        joystick.b().whileTrue(drivetrain.applyRequest(drivetrain.SnapToAngleRequest(joystick, 270)));
+        joystick.x().whileTrue(drivetrain.applyRequest(drivetrain.SnapToAngleRequest(joystick, 90)));
+        joystick.y().whileTrue(drivetrain.applyRequest(drivetrain.SnapToAngleRequest(joystick, 0)));
+        joystick.a().whileTrue(drivetrain.applyRequest(drivetrain.SnapToAngleRequest(joystick, 180)));
+        isNotMoving().whileTrue(drivetrain.applyRequest(drivetrain.pointWheelsAtRequest()));
 
-        joystick.rightTrigger()
-                .whileTrue(drivetrain.applyRequest(
-                        () -> robotCentric.withVelocityX(filterX.calculate(joystick.getLeftX() * MaxSpeed))
-                                .withVelocityY(-filterY.calculate(joystick.getLeftY() * MaxSpeed))
-                                .withRotationalRate(-joystick.getRightX() * MaxAngularRate)));
-
-        joystick.rightBumper().whileTrue(drivetrain.applyRequest(() -> brake));
+        joystick.rightTrigger().whileTrue(drivetrain.applyRequest(drivetrain.robotCentricRequest(joystick)));
+        joystick.rightBumper().whileTrue(drivetrain.applyRequest(drivetrain.brakeRequest()));
         // reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> {
             drivetrain.seedFieldRelative();
@@ -93,12 +50,6 @@ public class RobotContainer {
             drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
         }
         drivetrain.registerTelemetry(logger::telemeterize);
-    }
-
-    private Supplier<SwerveRequest> getSnapToAngleRequest(double degrees) {
-        return () -> snapToAngle.withVelocityX(filterX.calculate(joystick.getLeftX() * MaxSpeed))
-                .withVelocityY(-filterY.calculate(joystick.getLeftY() * MaxSpeed))
-                .withTargetDirection(Rotation2d.fromDegrees(degrees));
     }
 
     private Trigger isNotMoving() {
