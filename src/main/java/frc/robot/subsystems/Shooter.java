7dc71f8441b52b5c8f1d3ef5f314ed1d9d9f3a0e
Rishi <rishipriyanr@gmail.com>
2024-02-07 21:33:23 -0500

    torque based velocity

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,8 +4,13 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,6 +21,8 @@ import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
 
+
+
 private DoublePreferenceConstant leftShooterSpeed =
   new DoublePreferenceConstant("shooter/shooter/Leftspeed", 0);
 private DoublePreferenceConstant rightShooterSpeed =
@@ -23,9 +30,40 @@ private DoublePreferenceConstant rightShooterSpeed =
 
   final TalonFX m_LeftShooter = new TalonFX(8);
   final TalonFX m_RightShooter = new TalonFX(7);
+  private double talonFree = 6380;
+
+      TalonFXConfiguration configs = new TalonFXConfiguration();
+
+  private final VelocityTorqueCurrentFOC m_torqueVelocity = new VelocityTorqueCurrentFOC(0, 0, 0, 1, false, false, false);
+
+    m_robotContainer = new RobotContainer();
+
+
+    configs.Slot1.kP = 5; // An error of 1 rotation per second results in 5 amps output
+    configs.Slot1.kI = 0.1; // An error of 1 rotation per second increases output by 0.1 amps every second
+    configs.Slot1.kD = 0.001; // A change of 1000 rotation per second squared results in 1 amp output
+
+
+    configs.TorqueCurrent.PeakForwardTorqueCurrent = 40;
+    configs.TorqueCurrent.PeakReverseTorqueCurrent = -40;
+
+        StatusCode status_left = StatusCode.StatusCodeNotInitialized;
+         StatusCode status_right = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; ++i) {
+      status_left = m_LeftShooter.getConfigurator().apply(configs);
+       status_right = m_RightShooter.getConfigurator().apply(configs);
+      if (status_left.isOK() && status_right.isOK()) break;
+    }
+    if(!status_left.isOK() || !status_right.isOK()) {
+      System.out.println("Could not apply configs, error code: Left: " + status_left.toString() + "Right: " + status_right.toString());
+    }
+
+    m_LeftShooter.setControl(new Follower(m_RightShooter.getDeviceID(), false));
   
 
-private double talonFree = 6380;
+
+
+  
 
   public Shooter (){
     m_LeftShooter.setInverted(true);
@@ -33,8 +71,10 @@ private double talonFree = 6380;
   }
 
   public void startShooter() {
-    m_LeftShooter.set(leftShooterSpeed.getValue()/talonFree);
-    m_RightShooter.set(rightShooterSpeed.getValue()/talonFree);
+    m_LeftShooter.set(leftShooterSpeed.getValue() / talonFree);
+    m_RightShooter.set( rightShooterSpeed.getValue() / talonFree);
+
+
     }  
  
   public void stopShooter() {
@@ -52,7 +92,7 @@ private double talonFree = 6380;
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+   
     SmartDashboard.putNumber("Left Shooter Speed", m_LeftShooter.getVelocity().getValueAsDouble()*60);
     SmartDashboard.putNumber("Right Shooter Speed", m_RightShooter.getVelocity().getValueAsDouble()*60);
   }
