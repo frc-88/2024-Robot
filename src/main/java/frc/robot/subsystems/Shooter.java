b6e76b516a69fae88de810ebc57036df4058e0a6
Nick Avtges <navtges@gmail.com>
2024-02-08 19:13:36 -0500

    remove incomplete code

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,8 +10,6 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -20,8 +18,6 @@ import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
-
-
 
 private DoublePreferenceConstant leftShooterSpeed =
   new DoublePreferenceConstant("shooter/shooter/Leftspeed", 0);
@@ -36,35 +32,6 @@ private DoublePreferenceConstant rightShooterSpeed =
 
   private final VelocityTorqueCurrentFOC m_torqueVelocity = new VelocityTorqueCurrentFOC(0, 0, 0, 1, false, false, false);
 
-    m_robotContainer = new RobotContainer();
-
-
-    configs.Slot1.kP = 5; // An error of 1 rotation per second results in 5 amps output
-    configs.Slot1.kI = 0.1; // An error of 1 rotation per second increases output by 0.1 amps every second
-    configs.Slot1.kD = 0.001; // A change of 1000 rotation per second squared results in 1 amp output
-
-
-    configs.TorqueCurrent.PeakForwardTorqueCurrent = 40;
-    configs.TorqueCurrent.PeakReverseTorqueCurrent = -40;
-
-        StatusCode status_left = StatusCode.StatusCodeNotInitialized;
-         StatusCode status_right = StatusCode.StatusCodeNotInitialized;
-    for (int i = 0; i < 5; ++i) {
-      status_left = m_LeftShooter.getConfigurator().apply(configs);
-       status_right = m_RightShooter.getConfigurator().apply(configs);
-      if (status_left.isOK() && status_right.isOK()) break;
-    }
-    if(!status_left.isOK() || !status_right.isOK()) {
-      System.out.println("Could not apply configs, error code: Left: " + status_left.toString() + "Right: " + status_right.toString());
-    }
-
-    m_LeftShooter.setControl(new Follower(m_RightShooter.getDeviceID(), false));
-  
-
-
-
-  
-
   public Shooter (){
     m_LeftShooter.setInverted(true);
     m_RightShooter.setInverted(false);
@@ -73,8 +40,6 @@ private DoublePreferenceConstant rightShooterSpeed =
   public void startShooter() {
     m_LeftShooter.set(leftShooterSpeed.getValue() / talonFree);
     m_RightShooter.set( rightShooterSpeed.getValue() / talonFree);
-
-
     }  
  
   public void stopShooter() {
