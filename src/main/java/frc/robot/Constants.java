6cc2b85385ca909f7d77871bf517d7131c239674
Nick Avtges <navtges@gmail.com>
2024-02-11 11:26:58 -0500

    fix can ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,12 +52,12 @@ public final class Constants {
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 4;
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 15;
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 14;
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 8;
 
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7;
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8;
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 1;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 2;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 2;
 
     // Intake
