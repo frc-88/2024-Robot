c0d8e3a931fda72dae90df3161b022c44991bf27
Woz4tetra <woz4tetra@gmail.com>
2024-03-06 23:36:50 -0500

    use enum for power mode

--- src/main/java/frc/robot/ros/bridge/PowerModePublisher.java
+++ src/main/java/frc/robot/ros/bridge/PowerModePublisher.java
@@ -5,15 +5,27 @@ import frc.team88.ros.bridge.ROSNetworkTablesBridge;
 import frc.team88.ros.messages.std_msgs.RosInt32;
 
 public class PowerModePublisher implements Publisher {
-    private final BridgePublisher<RosInt32> powerModePub;
-    private int powerMode = 0;
+    public enum JetsonPowerMode {
+        UNKNOWN(-1),
+        LOW_POWER(3),
+        HIGH_POWER(6);
 
-    public PowerModePublisher(ROSNetworkTablesBridge bridge, int powerMode) {
+        public final int value;
+
+        private JetsonPowerMode(int value) {
+            this.value = value;
+        }
+    }
+
+    private final BridgePublisher<RosInt32> powerModePub;
+    private JetsonPowerMode powerMode = JetsonPowerMode.UNKNOWN;
+
+    public PowerModePublisher(ROSNetworkTablesBridge bridge, JetsonPowerMode powerMode) {
         powerModePub = new BridgePublisher<>(bridge, "power_mode");
         this.powerMode = powerMode;
     }
 
     public void publish() {
-        powerModePub.send(new RosInt32(this.powerMode));
+        powerModePub.send(new RosInt32(this.powerMode.value));
     }
 }
