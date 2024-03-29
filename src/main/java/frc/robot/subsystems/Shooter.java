ef50a2526fcfbb30be0486f8e7e68907230c583f
Nick Avtges <navtges@gmail.com>
2024-03-28 20:23:50 -0400

    adjust

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -109,8 +109,8 @@ public class Shooter extends SubsystemBase {
     }
 
     public boolean isShooterAtAlmostFullSpeed() {
-        return m_LeftShooter.getVelocity().getValueAsDouble() * 60 > leftShooterSpeed.getValue() - 1000
-                && m_RightShooter.getVelocity().getValueAsDouble() * 60 > rightShooterSpeed.getValue() - 1000;
+        return m_LeftShooter.getVelocity().getValueAsDouble() * 60 > leftShooterSpeed.getValue() - 1250
+                && m_RightShooter.getVelocity().getValueAsDouble() * 60 > rightShooterSpeed.getValue() - 1250;
     }
 
     public boolean isShooterAtAmpTrapSpeed() {
