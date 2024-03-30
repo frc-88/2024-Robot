b96f049eca6183005a52ac7bfbd48745a7fa0431
Aymen Abbassi <AymenNA2026@bridge-rayn.org>
2024-03-30 16:05:16 -0400

    has note coast mode

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -426,6 +426,7 @@ public class RobotContainer {
         }
 
         if (readyToCoast && hasNote) {
+            m_lights.disableLED();
             indexerStart = m_intake.getIndexerPosition();
             readyToCoast = false;
         }
